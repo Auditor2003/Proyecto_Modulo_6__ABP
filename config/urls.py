0668f42d0db03cacbb380dc5f2172e9f5a57efe3
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-18 20:23:57 -0300

    Conectar urls de la app tareas al proyecto principal

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Importo el panel de administración que trae Django
 from django.contrib import admin
-from django.urls import path
 
+# Importo las funciones necesarias para manejar rutas
+from django.urls import path, include
+
+
+# Aquí defino las rutas principales de todo mi proyecto
 urlpatterns = [
+
+    # Esta ruta permite acceder al panel de administración de Django
     path('admin/', admin.site.urls),
+
+    # Aquí conecto las URLs de mi aplicación "tareas"
+    # Esto le dice a Django que use el archivo tareas/urls.py
+    path('', include('tareas.urls')),
 ]
