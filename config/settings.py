f46f93051ac56ff308ac56e546a74bcc85c86788
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-19 22:48:39 -0300

    Configurar LOGIN_URL en settings para redirección de vistas protegidas con login_required

--- config/settings.py
+++ config/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Aquí defino la URL donde Django enviará a los usuarios cuando intenten
+# acceder a una vista protegida sin haber iniciado sesión
+LOGIN_URL = 'login'
